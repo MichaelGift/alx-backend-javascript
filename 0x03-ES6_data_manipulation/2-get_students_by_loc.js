1da094e9a786b88b2b65c0097993c1490b91d9eb
Michael Gift <simiyugift@outlook.com>
2024-07-02 17:37:31 +0300

    Add function docs

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,13 @@
+/**
+ * Retrieves students in a given location.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @param {String} city - The location.
+ * @returns - empty list or list by cities
+ */
 export default function getStudentsByLocation(students, city) {
   if (students instanceof Array) {
     return students.filter((student) => student.location === city);
